9da79e02b6f753ea7ddf0ac568c8adf76c2b3397
siipi sistemas <eljros999xxx@gmail.com>
2022-08-02 16:53:43 -0400

    test200

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -3,21 +3,25 @@ import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema({
     name: {
+        
         type: String,
         required: true,
         unique: true,
         trim: true
     },
     description: {
+
         type: String,
         trim: true
     },
     price: {
+
         type: Number,
         default: 0
     },
     
     image: {
+
         public_id: String,
         secure_url: String
     }
